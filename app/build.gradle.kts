ea46b227b98b900cb2e14c0dfddc5db6612220db
sergiob <sergiob@ciandt.com>
2023-12-18 21:41:30 -0300

    Add main screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,5 +68,4 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
-
 }
